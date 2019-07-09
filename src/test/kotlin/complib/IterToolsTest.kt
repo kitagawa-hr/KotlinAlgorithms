11cbc5bb1a20849382d1f23a74c7fc5a4c2a4838
kitagawa-hr <kitagawa@cancerscan.jp>
2019-07-09 17:23:17 +0900

    Add product to iterTools

--- src/test/kotlin/complib/IterToolsTest.kt
+++ src/test/kotlin/complib/IterToolsTest.kt
@@ -32,3 +32,18 @@ class UpperBoundTests {
         assertEquals(list.upperBound(10), 11)
     }
 }
+
+class ProductTests {
+    @Test
+    fun productTests() {
+        val res = product(listOf(1, 2), listOf(3, 4), listOf(5, 6)).iterator()
+        assertEquals(res.next(), listOf(1, 3, 5))
+        assertEquals(res.next(), listOf(1, 3, 6))
+        assertEquals(res.next(), listOf(1, 4, 5))
+        assertEquals(res.next(), listOf(1, 4, 6))
+        assertEquals(res.next(), listOf(2, 3, 5))
+        assertEquals(res.next(), listOf(2, 3, 6))
+        assertEquals(res.next(), listOf(2, 4, 5))
+        assertEquals(res.next(), listOf(2, 4, 6))
+    }
+}
