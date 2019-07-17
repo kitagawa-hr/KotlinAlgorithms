4ec04ae609a38106474d2cba8a546a83e77f75b5
kitagawa-hr <kitagawa@cancerscan.jp>
2019-07-17 18:47:33 +0900

    Refactor Graph.UnionFind

--- src/main/kotlin/complib/Graph.kt
+++ src/main/kotlin/complib/Graph.kt
@@ -2,24 +2,54 @@ package complib
 
 class UnionFind(n: Int) {
     data class Node(var parent: Int, var size: Int)
-    val nodes = Array<Node>(n) { Node(parent = it, size = 1) }
+    private val nodes = Array<Node>(n) { Node(parent = it, size = 1) }
     fun findRoot(x: Int): Int {
         if (x == this.nodes[x].parent) return x
         nodes[x].parent = findRoot(nodes[x].parent)
         return nodes[x].parent
     }
-    fun unite(x: Int, y: Int) {
-        var xRoot = findRoot(x)
-        var yRoot = findRoot(y)
-        if (xRoot == yRoot) return
-        if (getSize(xRoot) < getSize(yRoot)) {
-            nodes[xRoot].parent = yRoot
-            nodes[yRoot].size += nodes[xRoot].size
-        } else {
-            nodes[yRoot].parent = xRoot
-            nodes[xRoot].size += nodes[yRoot].size
-        }
+    fun unite(x: Int, y: Int): Boolean {
+        val xRoot = findRoot(x)
+        val yRoot = findRoot(y)
+        if (xRoot == yRoot) return false
+        val (smaller, bigger) = if (getSize(xRoot) < getSize(yRoot)) {
+            Pair(xRoot, yRoot)
+        } else { Pair(yRoot, xRoot) }
+        nodes[smaller].parent = bigger
+        nodes[bigger].size += nodes[smaller].size
+        return true
     }
+    fun getSize(x: Int) = nodes[findRoot(x)].size
+    fun isSame(x: Int, y: Int) = findRoot(x) == findRoot(y)
+}
+
+class WeightedUnionFind(n: Int) {
+    data class Node(var parent: Int, var size: Int, var weight_diff: Long)
+    private val nodes = Array<Node>(n) { Node(parent = it, size = 1, weight_diff = 0) }
+    fun findRoot(x: Int): Int {
+        if (x == this.nodes[x].parent) return x
+        nodes[x].parent = findRoot(nodes[x].parent)
+        nodes[x].weight_diff += nodes[nodes[x].parent].weight_diff
+        return nodes[x].parent
+    }
+    fun unite(x: Int, y: Int, weight: Long): Boolean {
+        val xRoot = findRoot(x)
+        val yRoot = findRoot(y)
+        if (xRoot == yRoot) return false
+        val (smaller, bigger) = if (getSize(xRoot) < getSize(yRoot)) {
+            Pair(xRoot, yRoot)
+        } else { Pair(yRoot, xRoot) }
+        val new_weight = weight + getWeight(xRoot) - getWeight(yRoot)
+        nodes[smaller].parent = bigger
+        nodes[bigger].size += nodes[smaller].size
+        nodes[smaller].weight_diff = if (smaller == xRoot) -new_weight else new_weight
+        return true
+    }
+    fun getWeight(x: Int): Long {
+        findRoot(x)
+        return nodes[x].weight_diff
+    }
+    fun getDiff(x: Int, y: Int) = getWeight(y) - getWeight(x)
     fun getSize(x: Int) = nodes[findRoot(x)].size
     fun isSame(x: Int, y: Int) = findRoot(x) == findRoot(y)
 }
--- src/test/kotlin/complib/GraphTest.kt
+++ src/test/kotlin/complib/GraphTest.kt
@@ -1,6 +1,7 @@
 package complib
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -9,8 +10,9 @@ class UnionFindTests {
     @Test
     fun uniteTest() {
         val uf = UnionFind(10)
-        uf.unite(1, 2)
-        uf.unite(1, 3)
+        assertTrue(uf.unite(1, 2))
+        assertTrue(uf.unite(1, 3))
+        // 1 -> (2, 3)
         assertEquals(uf.findRoot(1), 1)
         assertEquals(uf.findRoot(2), 1)
         assertEquals(uf.findRoot(3), 1)
@@ -20,10 +22,14 @@ class UnionFindTests {
         assertTrue(uf.isSame(1, 2))
         assertTrue(uf.isSame(2, 3))
         assertTrue(uf.isSame(3, 1))
-        uf.unite(4, 5)
-        uf.unite(5, 1)
+
+        assertTrue(uf.unite(4, 5))
+        assertTrue(uf.unite(5, 1))
+        // 1 -> (2, 3, 4, 5)
         assertEquals(uf.findRoot(4), 1)
         assertEquals(uf.findRoot(5), 1)
+        assertFalse(uf.unite(5, 1))
+        assertFalse(uf.unite(4, 1))
         assertEquals(uf.getSize(1), 5)
         assertEquals(uf.getSize(4), 5)
     }
