63e629df528fd7fc2f8adb92796ad65872532f99
kitagawa-hr <kitagawa@cancerscan.jp>
2019-07-25 14:40:48 +0900

    Add combinations and permutations and powerSet to IterTools

--- src/main/kotlin/complib/collections/IterTools.kt
+++ src/main/kotlin/complib/collections/IterTools.kt
@@ -1,8 +1,14 @@
 package complib.collections
 
+fun List<Int>.dot(other: List<Int>): Int =
+        this.mapIndexed { index, value -> value * other[index] }.sum()
+
+fun List<Long>.dot(other: List<Long>): Long =
+        this.mapIndexed { index, value -> value * other[index] }.sum()
+
 fun <T> product(vararg lists: List<T>): List<List<T>> {
     fun <T> oneDimTimesOne(left: List<T>, right: List<T>): List<List<T>> =
-        left.flatMap { lelem -> right.map { relem -> listOf(lelem, relem) } }
+            left.flatMap { lelem -> right.map { relem -> listOf(lelem, relem) } }
 
     fun <T> twoDimTimesOne(left: List<List<T>>, right: List<T>): List<List<T>> {
         val product = mutableListOf<List<T>>()
@@ -15,13 +21,41 @@ fun <T> product(vararg lists: List<T>): List<List<T>> {
     }
     require(lists.size >= 2)
     return lists
-        .drop(2)
-        .fold(oneDimTimesOne(lists[0], lists[1])) { acc, suc -> twoDimTimesOne(acc, suc) }
+            .drop(2)
+            .fold(oneDimTimesOne(lists[0], lists[1])) { acc, suc -> twoDimTimesOne(acc, suc) }
 }
 
-fun List<Int>.dot(other: List<Int>): Int =
-        this.mapIndexed { index, value -> value * other[index] }.sum()
 
-fun List<Long>.dot(other: List<Long>): Long =
-        this.mapIndexed { index, value -> value * other[index] }.sum()
+fun <T> Collection<T>.powerSet(): Set<Set<T>> = powerSet(this, setOf(setOf()))
 
+private tailrec fun <T> powerSet(left: Collection<T>, acc: Set<Set<T>>): Set<Set<T>> = when {
+    left.isEmpty() -> acc
+    else -> powerSet(left.drop(1), acc + acc.map { it + left.first() })
+}
+
+fun <T> Set<T>.combinations(n: Int): Set<Set<T>> = when {
+    n < 0 -> throw Error("n must be positive, but got $n")
+    n == 0 -> setOf(setOf())
+    n >= size -> setOf(toSet())
+    else -> powerSet()
+            .filter { it.size == n }
+            .toSet()
+}
+
+fun <T> List<T>.insert(index: Int, element: T): List<T> = when {
+    index < 0 || index > size -> throw Error("Can not insert to index = $index")
+    index == 0 -> listOf(element) + this
+    index == size -> this + element
+    else -> dropLast(size - index) + element + drop(index)
+}
+
+fun <T> List<T>.permutations(): Set<List<T>> = when {
+    isEmpty() -> setOf()
+    size == 1 -> setOf(listOf(get(0)))
+    else -> {
+        val element = get(0)
+        drop(1).permutations()
+                .flatMap { sublist -> (0..sublist.size).map { i -> sublist.insert(i, element) } }
+                .toSet()
+    }
+}
--- src/test/kotlin/complib/collections/IterToolsTest.kt
+++ src/test/kotlin/complib/collections/IterToolsTest.kt
@@ -3,34 +3,119 @@ package complib.collections
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class ProductTests {
-    @Test
-    fun productTests() {
-        val res = product(listOf(1, 2), listOf(3, 4), listOf(5, 6)).iterator()
-        assertEquals(res.next(), listOf(1, 3, 5))
-        assertEquals(res.next(), listOf(1, 3, 6))
-        assertEquals(res.next(), listOf(1, 4, 5))
-        assertEquals(res.next(), listOf(1, 4, 6))
-        assertEquals(res.next(), listOf(2, 3, 5))
-        assertEquals(res.next(), listOf(2, 3, 6))
-        assertEquals(res.next(), listOf(2, 4, 5))
-        assertEquals(res.next(), listOf(2, 4, 6))
-    }
-}
-
 internal class DotTest {
 
     @Test
     fun dotInt() {
         val v1 = listOf(1, 2, 3)
         val v2 = listOf(1, 3, 5)
-        assertEquals(v1.dot(v2), 1 * 1 + 2 * 3 + 3 * 5)
+        assertEquals(1 * 1 + 2 * 3 + 3 * 5, v1.dot(v2))
     }
 
     @Test
     fun dotLong() {
         val v1 = listOf(1L, 2L, 3L)
         val v2 = listOf(1L, 3L, 5L)
-        assertEquals(v1.dot(v2), 1L * 1L + 2L * 3L + 3L * 5L)
+        assertEquals(1L * 1L + 2L * 3L + 3L * 5L, v1.dot(v2))
+    }
+}
+
+internal class ProductTests {
+    @Test
+    fun productTests() {
+        val res = product(listOf(1, 2), listOf(3, 4), listOf(5, 6)).iterator()
+        assertEquals(listOf(1, 3, 5), res.next())
+        assertEquals(listOf(1, 3, 6), res.next())
+        assertEquals(listOf(1, 4, 5), res.next())
+        assertEquals(listOf(1, 4, 6), res.next())
+        assertEquals(listOf(2, 3, 5), res.next())
+        assertEquals(listOf(2, 3, 6), res.next())
+        assertEquals(listOf(2, 4, 5), res.next())
+        assertEquals(listOf(2, 4, 6), res.next())
+    }
+}
+
+
+internal class CombinationsTests {
+    @Test
+    fun combinationsTest1() {
+        val res1 = setOf(1, 2, 3).combinations(2)
+        val expected1 = setOf(
+                setOf(1, 2),
+                setOf(1, 3),
+                setOf(2, 3)
+        )
+        assertEquals(expected1, res1)
+    }
+
+    @Test
+    fun combinationsTest2() {
+        val res2 = setOf(1, 2, 3, 4).combinations(3)
+        val expected2 = setOf(
+                setOf(1, 2, 3),
+                setOf(1, 2, 4),
+                setOf(1, 3, 4),
+                setOf(2, 3, 4)
+        )
+        assertEquals(expected2, res2)
+    }
+}
+
+internal class PowerSetTest {
+    @Test
+    fun powerSetTest() {
+        val res = setOf(1, 2, 3).powerSet()
+        val expected = setOf(
+                setOf(),
+                setOf(1),
+                setOf(2),
+                setOf(3),
+                setOf(1, 2),
+                setOf(1, 3),
+                setOf(2, 3),
+                setOf(1, 2, 3)
+        )
+        assertEquals(expected, res)
+
+    }
+}
+
+internal class InsertTest {
+    @Test
+    fun insertToHead() {
+        val res = mutableListOf(2, 3, 4).insert(0, 1)
+        val expected = mutableListOf(1, 2, 3, 4)
+        assertEquals(expected, res)
+    }
+
+    @Test
+    fun insertToTail() {
+        val res = mutableListOf(2, 3, 4).insert(3, 1)
+        val expected = mutableListOf(2, 3, 4, 1)
+        assertEquals(expected, res)
+    }
+
+    @Test
+    fun insertToBody() {
+        val res = mutableListOf(2, 3, 4).insert(1, 1)
+        val expected = mutableListOf(2, 1, 3, 4)
+        assertEquals(expected, res)
+    }
+}
+
+internal class PermutationsTests {
+    @Test
+    fun permutationsTest() {
+        val res2 = listOf(1, 2, 3).permutations()
+        val expected2 = setOf(
+                listOf(1, 2, 3),
+                listOf(1, 3, 2),
+                listOf(2, 1, 3),
+                listOf(2, 3, 1),
+                listOf(3, 1, 2),
+                listOf(3, 2, 1)
+        )
+        assertEquals(expected2, res2)
+
     }
 }
