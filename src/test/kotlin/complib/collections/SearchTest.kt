6ed5ac51a8ecfb056d02fa9ab762c4b2e52b3df7
kitagawa-hr <kitagawa@cancerscan.jp>
2019-08-01 13:33:35 +0900

    Refactor test

--- src/test/kotlin/complib/collections/SearchTest.kt
+++ src/test/kotlin/complib/collections/SearchTest.kt
@@ -7,30 +7,30 @@ import org.junit.jupiter.api.Assertions.*
 class LowerBoundTests {
 
     @Test
-    fun gcdIntTest() {
+    fun lowerBoundTest() {
         val list = listOf<Int>(1, 1, 2, 2, 3, 5, 6, 7, 8, 8, 9)
-        assertEquals(list.lowerBound(0), 0)
-        assertEquals(list.lowerBound(1), 0)
-        assertEquals(list.lowerBound(2), 2)
-        assertEquals(list.lowerBound(3), 4)
-        assertEquals(list.lowerBound(8), 8)
-        assertEquals(list.lowerBound(9), 10)
-        assertEquals(list.lowerBound(10), 11)
+        assertEquals(0, list.lowerBound(0))
+        assertEquals(0, list.lowerBound(1))
+        assertEquals(2, list.lowerBound(2))
+        assertEquals(4, list.lowerBound(3))
+        assertEquals(8, list.lowerBound(8))
+        assertEquals(10, list.lowerBound(9))
+        assertEquals(11, list.lowerBound(10))
     }
 }
 
 class UpperBoundTests {
 
     @Test
-    fun gcdIntTest() {
+    fun upperBoundTest() {
         val list = listOf<Int>(1, 1, 2, 2, 3, 5, 6, 7, 8, 8, 9)
-        assertEquals(list.upperBound(0), 0)
-        assertEquals(list.upperBound(1), 2)
-        assertEquals(list.upperBound(2), 4)
-        assertEquals(list.upperBound(3), 5)
-        assertEquals(list.upperBound(8), 10)
-        assertEquals(list.upperBound(9), 11)
-        assertEquals(list.upperBound(10), 11)
+        assertEquals(0, list.upperBound(0))
+        assertEquals(2, list.upperBound(1))
+        assertEquals(4, list.upperBound(2))
+        assertEquals(5, list.upperBound(3))
+        assertEquals(10, list.upperBound(8))
+        assertEquals(11, list.upperBound(9))
+        assertEquals(11, list.upperBound(10))
     }
 }
 
