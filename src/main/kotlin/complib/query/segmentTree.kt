616b4a40f2f85a8814934e7a4eb293860ab1f4ce
kitagawa-hr <kitagawa@cancerscan.jp>
2019-11-27 16:45:31 +0900

    Add query.SegmentTree Verified at AOJ. (http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=4010450#1)

---
+++ src/main/kotlin/complib/query/segmentTree.kt
@@ -0,0 +1,49 @@
+package complib.query
+
+import complib.math.Monoid
+
+
+class SegmentTree<T>(array: Array<T>, private val monoid: Monoid<T>) {
+    private val n = array.size
+    private val tree = MutableList(2 * n) {
+        if (it >= n) array[it - n] else monoid.empty()
+    }
+
+    init {
+        for (i in n - 1 downTo 0) {
+            tree[i] = monoid.combine(tree[2 * i], tree[2 * i + 1])
+        }
+    }
+
+    fun get(index: Int) = tree[index + n]
+    fun query(left: Int, right: Int): T {
+        // [left, right)
+        require(left in (0 until right) && right <= n) {
+            "Invalid Range [$left, $right)"
+        }
+        var (tl, tr) = left + n to right + n
+        var ret = monoid.empty()
+        while (tl < tr) {
+            if (tl % 2 == 1) {
+                ret = monoid.combine(ret, tree[tl])
+                tl++
+            }
+            if (tr % 2 == 1) {
+                tr--
+                ret = monoid.combine(ret, tree[tr])
+            }
+            tl /= 2
+            tr /= 2
+        }
+        return ret
+    }
+
+    fun update(index: Int, value: T) {
+        var tIndex = index + n
+        tree[tIndex] = value
+        while (tIndex > 1) {
+            tIndex /= 2
+            tree[tIndex] = monoid.combine(tree[2 * tIndex], tree[2 * tIndex + 1])
+        }
+    }
+}
---
+++ src/test/kotlin/complib/query/segmentTreeTest.kt
@@ -0,0 +1,50 @@
+package complib.query
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+import complib.math.IntMonoid
+import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
+
+internal class SegmentTreeSumTests {
+    private val array = arrayOf(1, 3, 5, 2, 9, 12, 8, 13, 15)
+    private val sumQuery = SegmentTree(array, IntMonoid)
+
+    @ParameterizedTest(name = "sum of [{0}, {1}) = {2}")
+    @CsvSource(
+            "0,    1,   1",
+            "0,    2,   4",
+            "0,    3,   9",
+            "1,    3,   8",
+            "0,    9,  68",
+            "3,    8,   44"
+    )
+    fun queryTest(from: Int, to: Int, min: Int) {
+        assertEquals(min, sumQuery.query(from, to))
+    }
+
+    @ParameterizedTest(name = "invalid query range should throw Exception")
+    @CsvSource(
+            "0,    0",
+            "-1,    2",
+            "0,    11"
+    )
+    fun queryRaisesTest(from: Int, to: Int) {
+        assertThrows(IllegalArgumentException::class.java) {
+            sumQuery.query(from, to)
+        }
+    }
+
+    @Test
+    fun updateTest(){
+        sumQuery.update(0, 10)
+        // 10, 3, 5, 2, 9, 12, 8, 13, 15
+        assertEquals(18, sumQuery.query(0, 3))
+        sumQuery.update(3, -8)
+        // 10, 3, 5, -8, 9, 12, 8, 13, 15
+        assertEquals(18, sumQuery.query(0, 3))
+        assertEquals(10, sumQuery.query(0, 4))
+    }
+}
