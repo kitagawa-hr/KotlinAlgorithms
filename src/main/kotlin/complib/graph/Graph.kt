a2a477b96826a1aca9a3c46a6c0c2e4d037236eb
kitagawa-hr <kitagawa@cancerscan.jp>
2019-12-24 16:55:39 +0900

    Update graph.Graph

--- src/main/kotlin/complib/graph/Graph.kt
+++ src/main/kotlin/complib/graph/Graph.kt
@@ -1,11 +1,19 @@
 package complib.graph
 
-data class Vertex<T : Comparable<T>, E : Comparable<E>>(val data: T? = null) {
-    data class Edge<T : Comparable<T>>(val to: Int, val weight: T? = null)
+interface Graph<T : Comparable<T>, E : Comparable<E>> {
+    val size: Int
+    fun getWeight(from: Int, to: Int): E?
+    fun edgesFrom(from: Int): List<Edge<E>>
+    fun addEdge(from: Int, to: Int, weight: E): Boolean
+    fun removeEdge(from: Int, to: Int): Boolean
+    fun isAdjacent(from: Int, to: Int): Boolean
+}
 
+data class Edge<T : Comparable<T>>(val to: Int, val weight: T)
+data class Vertex<T : Comparable<T>, E : Comparable<E>>(val data: T) {
     val edges = mutableListOf<Edge<E>>()
 
-    fun addEdge(to: Int, weight: E?) = when (edges.find { it.to == to }) {
+    fun addEdge(to: Int, weight: E) = when (edges.find { it.to == to }) {
         null -> {
             edges.add(Edge(to, weight))
             true
@@ -25,10 +33,43 @@ data class Vertex<T : Comparable<T>, E : Comparable<E>>(val data: T? = null) {
 }
 
 class AdjacentListGraph<T : Comparable<T>, E : Comparable<E>>(
-    val vertices: MutableList<Vertex<T, E>>
-) {
-    fun getWeight(from: Int, to: Int): E? = vertices[from].edges.find { it.to == to }?.weight
-    fun addEdge(from: Int, to: Int, weight: E? = null) = vertices[from].addEdge(to, weight)
-    fun removeEdge(from: Int, to: Int) = vertices[from].removeEdge(to)
-    fun isAdjacent(from: Int, to: Int) = vertices[from].edges.find { it.to == to } != null
+    private val vertices: MutableList<Vertex<T, E>>
+) : Graph<T, E> {
+    override val size = vertices.size
+    override fun getWeight(from: Int, to: Int): E? =
+        vertices[from].edges.find { it.to == to }?.weight
+    override fun edgesFrom(from: Int): List<Edge<E>> = vertices[from].edges
+    override fun addEdge(from: Int, to: Int, weight: E) = vertices[from].addEdge(to, weight)
+    override fun removeEdge(from: Int, to: Int) = vertices[from].removeEdge(to)
+    override fun isAdjacent(from: Int, to: Int) =
+        vertices[from].edges.find { it.to == to } != null
+}
+
+class AdjacencyMatrixGraph<T : Comparable<T>, E : Comparable<E>>(
+    private val matrix: Array<Array<E?>>
+) : Graph<T, E> {
+    override val size = matrix.size
+    override fun getWeight(from: Int, to: Int): E? = matrix[from][to]
+    override fun edgesFrom(from: Int): List<Edge<E>> =
+        matrix[from].withIndex()
+            .filter { it.value != null }
+            .map { Edge(it.index, it.value!!) }
+
+    override fun addEdge(from: Int, to: Int, weight: E): Boolean {
+        if (matrix[from][to] == null) {
+            matrix[from][to] = weight
+            return true
+        }
+        return false
+    }
+
+    override fun removeEdge(from: Int, to: Int): Boolean {
+        if (from >= size || to >= size || matrix[from][to] == null) {
+            return false
+        }
+        matrix[from][to] = null
+        return true
+    }
+
+    override fun isAdjacent(from: Int, to: Int): Boolean = matrix[from][to] != null
 }
--- src/test/kotlin/complib/graph/GraphTest.kt
+++ src/test/kotlin/complib/graph/GraphTest.kt
@@ -9,7 +9,7 @@ internal class GraphTest {
     @Nested
     inner class AdjacentListGraphTest {
         private val g = AdjacentListGraph(
-            MutableList(5) { Vertex<Int, Int>() }
+            MutableList(5) { Vertex<Int, Int>(it) }
         )
 
         @BeforeEach
@@ -39,7 +39,7 @@ internal class GraphTest {
             4 to listOf()
         ).map { (input, expected) ->
             DynamicTest.dynamicTest("edges from $input should be $expected") {
-                assertEquals(expected, g.vertices[input].edges.map { it.to })
+                assertEquals(expected, g.edgesFrom(input).map { it.to })
             }
         }
 
@@ -57,6 +57,7 @@ internal class GraphTest {
         fun testGetWeight(from: Int, to: Int, expected: Int?) {
             assertEquals(expected, g.getWeight(from, to))
         }
+
         @ParameterizedTest(name = "There is edge from {0} to {1} == {2}")
         @CsvSource(
             "0, 1, true",
@@ -71,6 +72,7 @@ internal class GraphTest {
         fun testIsAdjacent(from: Int, to: Int, expected: Boolean) {
             assertEquals(expected, g.isAdjacent(from, to))
         }
+
         @Test
         fun testRemoveEdge() {
             """
@@ -92,7 +94,101 @@ internal class GraphTest {
                 4 to listOf()
             ).forEach { (input, expected) ->
                 DynamicTest.dynamicTest("neighbor of $input should be $expected") {
-                    assertEquals(expected, g.vertices[input].edges.map { it.to })
+                    assertEquals(expected, g.edgesFrom(input).map { it.to })
+                }
+            }
+        }
+    }
+
+    @Nested
+    inner class AdjacencyMatrixGraphTest {
+        private val g = AdjacencyMatrixGraph<Int, Int>(
+            Array(5) { Array<Int?>(5) { null } }
+        )
+
+        @BeforeEach
+        fun setUp() {
+            """
+            0 ---> 1
+            |   /  | ^
+            |  /   |  2
+            v v    v v
+            4 <---  3
+            """.trimIndent()
+            g.addEdge(0, 1, 1)
+            g.addEdge(0, 4, 4)
+            g.addEdge(1, 3, 2)
+            g.addEdge(1, 4, 3)
+            g.addEdge(2, 1, 1)
+            g.addEdge(2, 3, 1)
+            g.addEdge(3, 4, 1)
+        }
+
+        @TestFactory
+        fun testGetEdges() = listOf(
+            0 to listOf(1, 4),
+            1 to listOf(3, 4),
+            2 to listOf(1, 3),
+            3 to listOf(4),
+            4 to listOf()
+        ).map { (input, expected) ->
+            DynamicTest.dynamicTest("edges from $input should be $expected") {
+                assertEquals(expected, g.edgesFrom(input).map { it.to })
+            }
+        }
+
+        @ParameterizedTest(name = "Weight of edge from {0} to {1} is {2}")
+        @CsvSource(
+            "0, 1, 1",
+            "0, 4, 4",
+            "2, 3, 1",
+            "1, 2, ",
+            "0, 2, ",
+            "0, 3, ",
+            "2, 4, ",
+            "2, 0, "
+        )
+        fun testGetWeight(from: Int, to: Int, expected: Int?) {
+            assertEquals(expected, g.getWeight(from, to))
+        }
+
+        @ParameterizedTest(name = "There is edge from {0} to {1} == {2}")
+        @CsvSource(
+            "0, 1, true",
+            "0, 4, true",
+            "2, 3, true",
+            "1, 2, false",
+            "0, 2, false",
+            "0, 3, false",
+            "2, 4, false",
+            "2, 0, false"
+        )
+        fun testIsAdjacent(from: Int, to: Int, expected: Boolean) {
+            assertEquals(expected, g.isAdjacent(from, to))
+        }
+
+        @Test
+        fun testRemoveEdge() {
+            """
+            0 ---> 1
+                / |
+               /  |  2
+              v   v v
+            4 <--- 3
+            """.trimIndent()
+            assertEquals(true, g.removeEdge(0, 4))
+            assertEquals(false, g.removeEdge(0, 4))
+            assertEquals(true, g.removeEdge(2, 1))
+            assertEquals(false, g.removeEdge(1, 5))
+            listOf(
+                0 to listOf(1),
+                1 to listOf(3),
+                2 to listOf(3),
+                3 to listOf(4),
+                4 to listOf()
+            ).forEach { (input, expected) ->
+                DynamicTest.dynamicTest("neighbor of $input should be $expected") {
+                    assertEquals(expected, g.edgesFrom(input).map { it.to })
                 }
             }
         }
